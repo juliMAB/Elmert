17a82dbbf07b2c0ba652107233f665bff8b7790a
juliMAB <julian6aguirre@hotmail.com>
2022-01-28 23:27:40 -0300

    Player Move with Animation.

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -6,27 +6,23 @@ public class CharacterMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
 
-    private Rigidbody rb;
+    [SerializeField] private Rigidbody rb;
 
-    private Animator animator;
+    [SerializeField] private Animator animator;
 
     // Start is called before the first frame update
 
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody>();
-        animator = GetComponent<Animator>();
-    }
     protected void AnimatorUpdate(Vector3 dir)
     {
         animator.SetFloat("Horizontal", dir.x);
         animator.SetFloat("Vertical", dir.y);
         animator.SetFloat("Speed", dir.sqrMagnitude);
+        
     }
 
    
     protected void Move(Vector3 dir)
     {
-        rb.MovePosition(rb.position + (dir * moveSpeed * Time.fixedDeltaTime));
+        rb.MovePosition(rb.position + (dir.normalized * moveSpeed * Time.fixedDeltaTime));
     }
 }
