0c009a872f4d656054f2fda3bea6965dffef6d81
guillermina <manzanog0612@gmail.com>
2022-01-28 22:10:48 -0300

    Added basic lintern fuctionality.

---
+++ Assets/Scripts/LinternController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinternController : MonoBehaviour
+{
+    [SerializeField] private GameObject cuteView = null;
+    [SerializeField] private GameObject darkView = null;
+
+    private void Start()
+    {
+        TurnViewOn(true);
+    }
+
+    private void Update()
+    {
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorldPosition.z = 0;
+
+        Vector3 directonToLook = mouseWorldPosition - transform.position;
+        transform.up = directonToLook;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            TurnViewOn(true);
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            TurnViewOn(false);
+        }
+    }
+
+    private void TurnViewOn(bool isCuteView)
+    {
+        cuteView.SetActive(isCuteView);
+        darkView.SetActive(!isCuteView);
+    }
+}
