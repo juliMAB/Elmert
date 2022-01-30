014271d4a63f77ae63a4ac3ef151f6fc1739c055
juliMAB <julian6aguirre@hotmail.com>
2022-01-29 22:11:20 -0300

    Make Better background.

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -19,10 +19,6 @@ public class CharacterMovement : MonoBehaviour
             animator.SetFloat("Horizontal", dir.x);
             animator.SetFloat("Vertical", dir.y);
             animator.SetFloat("Speed", dir.sqrMagnitude);
-            if (dir.x != 0)
-            {
-                transform.localScale = new Vector3(dir.x, transform.localScale.y, transform.localScale.y);
-            }
         }
     }
 
